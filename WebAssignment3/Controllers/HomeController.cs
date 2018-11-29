d3cd770354e78281b4c88ebd5524a9c996bfeafb
LandSprutte <christianbondesen@gmail.com>
2018-11-29 21:05:32 +0100

    Able to display a list of component types for a given category

--- WebAssignment3/Controllers/HomeController.cs
+++ WebAssignment3/Controllers/HomeController.cs
@@ -3,17 +3,50 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebAssignment3.Data;
 using WebAssignment3.Models;
+using WebAssignment3.ViewModels;
 
 namespace WebAssignment3.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public HomeController(ApplicationDbContext context, IMapper mapper) {
+            _context = context;
+            _mapper = mapper;
         }
 
+        public IActionResult Index()
+        {
+            HomeViewModel vm = new HomeViewModel
+            {
+                CategoriesSelect = new SelectList(_context.Category.ToList(), "CategoryId", "Name"),
+                ComponentTypesSelect = new SelectList(_context.ComponentType.ToList(), "ComponentTypeId", "ComponentName")
+
+            };
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(HomeViewModel vm)
+        {
+            var componentTypeCatsFromDb = _context.ComponentTypeCategory.Where(ctc => ctc.CategoryId == vm.SelectedCategoryId).ToList();
+
+            foreach (var ctc in componentTypeCatsFromDb)
+            {
+                var tempComponentType = _mapper.Map<ComponentTypeViewModel>(_context.ComponentType.Find(ctc.ComponentTypeId));
+                vm.ComponentTypes.Add(tempComponentType);
+            }
+
+            vm.CategoriesSelect = new SelectList(_context.Category.ToList(), "CategoryId", "Name");
+
+            return View(vm);
+        }
     }   
 }
---
+++ WebAssignment3/ViewModels/HomeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAssignment3.Models;
+
+namespace WebAssignment3.ViewModels
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel()
+        {
+            ComponentTypes = new List<ComponentTypeViewModel>();
+            Components = new List<Component>();
+        }
+
+        [Display(Name = "Categories")]
+        public SelectList CategoriesSelect { get; set; }
+
+        [Display(Name ="Component Types")]
+        public SelectList ComponentTypesSelect { get; set; }
+
+        public long SelectedCategoryId { get; set; }
+
+        public long SelectedComponentTypeId { get; set; }
+
+        [Display(Name ="Component Types")]
+        public List<ComponentTypeViewModel> ComponentTypes { get; set; }
+
+        [Display(Name = "Components")]
+        public List<Component> Components { get; set; }
+    }
+}
